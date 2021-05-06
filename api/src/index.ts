f5b22b8eceece327e97bae062354b7bd23936d03
Craig Russell <craig@craig-russell.co.uk>
2021-05-06 17:05:17 +0100

    Added minimal Express API

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import * as express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({message: 'Hello World!'});
+});
+
+const port = 1337;
+const host = '0.0.0.0';
+
+app.listen(port, host, () => {
+    console.log(`Listening on http://${host}:${port}`);
+});
