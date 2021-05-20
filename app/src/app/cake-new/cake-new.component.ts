4912d4a267d7ed143d88e063d2e0083708f6cc8a
Craig Russell <craig@craig-russell.co.uk>
2021-05-20 14:57:40 +0100

    Added form input validation

--- app/src/app/cake-new/cake-new.component.ts
+++ app/src/app/cake-new/cake-new.component.ts
@@ -12,15 +12,58 @@ export class CakeNewComponent implements OnInit {
 
   cake: Cake = new Cake();
 
+  nameError: string;
+  commentError: string;
+  imageUrlError: string;
+  yumFactorError: string;
+
   constructor(
     private router: Router,
     private cakeServices: CakesService
   ) { }
 
   ngOnInit(): void {
+    this.clearErrors();
+  }
+
+  clearErrors(){
+    this.nameError = undefined;
+    this.commentError = undefined;
+    this.imageUrlError = undefined;
+    this.yumFactorError = undefined;
+  }
+
+  validate(){
+    this.clearErrors();
+
+    // Validate name
+    if ( !this.cake.name || this.cake.name.length === 0 ) { this.nameError = 'Name is required' }
+    
+    // Validate comment
+    if ( !this.cake.comment ) { 
+      this.commentError = 'Comment is required'; 
+    } else {
+      if ( this.cake.comment.length < 5 ) { this.commentError = 'Comment must be greater than 5 characters' }
+      if ( this.cake.comment.length > 200 ) { this.commentError = 'Comment must be less than 200 characters' }
+    }
+
+    // Validate image url
+    if ( !this.cake.imageUrl ) { this.imageUrlError = 'Image URL s required' }
+
+    // Validate yum factor
+    if ( !this.cake.yumFactor ) { 
+      this.yumFactorError = 'Yum factor is required'; 
+    } else {
+      if ( this.cake.yumFactor < 1 ) { this.yumFactorError = 'Yum factor must be between 1 and 5' }
+      if ( this.cake.yumFactor > 5 ) { this.yumFactorError = 'Yum factor must be between 1 and 5' }
+    }
+    
+    return !!( this.nameError || this.commentError || this.imageUrlError || this.yumFactorError );
   }
 
   create(){
+    if ( this.validate() ) { return }
+
     this.cakeServices.create(this.cake).subscribe(c => {
       this.router.navigate(['cakes']);
     });
